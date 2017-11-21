ae7f534f2138e86512f3721841a35e082b927111
weinbe58 <weinbe58@bu.edu>
2017-11-21 08:46:03 -0500

    adding parameters for qaqmc function

--- qaqmc_local.h
+++ qaqmc_local.h
@@ -8,14 +8,16 @@
 class qaqmc_local : public proj_base{
 	protected:
 		const int * bst;
+		int * ipar;
+		double * rpar;
 		const int Nb;
 		const double Smax;
 		double beta;
-		double (*S_func)(double);
+		double (*S_func)(double,double*,int*);
 		void move_op(int);
 
 	public:
-		qaqmc_local(int,const int,const int, const int[], double (*)(double), const int, const int,
+		qaqmc_local(int,const int,const int, const int[],double*,int*,double (*)(double,double*,int*), const int, const int,
 			const std::vector<signed char>,const std::vector<signed char>);
 		~qaqmc_local() {};
 		int get_Nb() {return Nb;}
@@ -23,9 +25,9 @@ class qaqmc_local : public proj_base{
 };
 
 
-qaqmc_local::qaqmc_local(int _M,const int _N,const int _Nb, const int _bst[],double (* func)(double),
+qaqmc_local::qaqmc_local(int _M,const int _N,const int _Nb, const int _bst[],double *_rpar,int *_ipar,double (* func)(double,double*,int*),
 		 const int _Fl, const int _Fr, const std::vector<signed char> _sL,const std::vector<signed char> _sR) :
-proj_base::proj_base(_M,_N,_Fl,_Fr,_sL,_sR), Nb(_Nb), bst(_bst), S_func(func), Smax(func(1.0)){
+proj_base::proj_base(_M,_N,_Fl,_Fr,_sL,_sR), rpar(_rpar), ipar(_ipar), Nb(_Nb), bst(_bst), S_func(func), Smax(func(1.0,_rpar,_ipar)){
 	for(int i=0;i<2*Nb;i++){
 		if(bst[i]<0 || bst[i]>=_N){
 			std::cout << "bond index out of bounds" << std::endl;
@@ -38,10 +40,10 @@ proj_base::proj_base(_M,_N,_Fl,_Fr,_sL,_sR), Nb(_Nb), bst(_bst), S_func(func), S
 void qaqmc_local::move_op(int p){
 	double S;
 	if(p < base::M/2){
-		S = S_func(2*double(p+1)/base::M);
+		S = S_func(2*double(p+1)/base::M,rpar,ipar);
 	}
 	else{
-		S = Smax-S_func(2*double(p-base::M/2)/base::M);
+		S = Smax-S_func(2*double(p-base::M/2)/base::M,rpar,ipar);
 	}
 	double W = 2*Nb*S + base::N*(1-S);;
 	while(true){
